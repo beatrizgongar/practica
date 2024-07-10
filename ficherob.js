2dfddf9d1d0562fe94c2cec889e9b2c7c94ece09
Bea Gonzalez <beatrizgongar@yahoo.es>
2024-07-10 20:22:53 +0200

    mezclando mirama

--- ficherob.js
+++ ficherob.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-console.log("*****************");
-console.log("soy el fichero II");
-console.log("*****************");
-=======
 console.log("-----------------");
 console.log("soy el segundo fichero");
 console.log("-----------------");
->>>>>>> mirama
